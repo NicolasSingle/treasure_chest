705d9b03229a2d0dbf52787e04c10a8eaf28d472
Gnak <523748995@qq.com>
2019-09-06 17:06:04 +0800

    vue指令/组件/transition相关

---
+++ Vue/transitions/淡入淡出/Fade.vue
@@ -0,0 +1,35 @@
+<template functional>
+  <transition
+    name="commonFade"
+    :mode="props.mode"
+    @after-leave="props.onTransitionAfterLeave"
+  >
+    <slot></slot>
+  </transition>
+</template>
+<style lang='scss'>
+.commonFade-enter-active,
+.commonFade-leave-active {
+  transition: opacity .15s ease-in-out;
+}
+.commonFade-enter,
+.commonFade-leave-to {
+  opacity: 0;
+}
+</style>
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      validator: value => {
+        return ["out-in", "in-out"].includes(value);
+      }
+    },
+    onTransitionAfterLeave: {
+      type: Function,
+      default: () => {}
+    }
+  }
+};
+</script>
---
+++ Vue/组件/input输入框/谷歌风格的输入框/google-input/index.js
@@ -0,0 +1,9 @@
+import GoogleInput from "./GoogleInput.vue"
+
+const GooInput = {
+  install: function (Vue) {
+    Vue.component('google-input', GoogleInput)
+  }
+}
+
+export default GooInput;
